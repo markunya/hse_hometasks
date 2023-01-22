2e70200aee482c48df8398fb894675b9ad871fe2
Mark Bondarenko <vasya@pupkin.ru>
2023-01-22 13:45:39 +0300

    Triple for

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,5 +1,18 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    return false;
+    int n = table.size();
+    for (int i = 0; i < n; ++i) {
+        if (table[i].size() != n) {
+            return false;
+        }
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                if (table[table[i][j]][k] != table[i][table[j][k]]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
 }
