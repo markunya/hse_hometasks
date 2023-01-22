393783a2982e61d67b9d27da66fe67c1d7abb936
Mark Bondarenko <vasya@pupkin.ru>
2023-01-22 14:09:33 +0300

    first attempt of palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,23 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    int left = 0;
+    int right = str.length() - 1;
+    while (left < right) {
+        if (str[left] == ' ') {
+            ++left;
+            continue;
+        }
+        if(str[right] == ' ') {
+            --right;
+            continue;
+        }
+        if (str[left] != str[right]) {
+            return false;
+        } else {
+            ++left;
+            --right;
+        }
+    }
+    return true;
 }
