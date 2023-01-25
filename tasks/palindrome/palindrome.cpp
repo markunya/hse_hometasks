e90e343fd066f81153adf717cc981c4f4a095cb3
Mark Bondarenko <vasya@pupkin.ru>
2023-01-25 18:36:11 +0300

    delete some space

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -3,7 +3,7 @@
 bool IsPalindrome(const std::string& str) {
     size_t length_of_palindrome = str.size();
     size_t left = 0;
-    size_t right =  length_of_palindrome == 0 ? 0 : length_of_palindrome - 1;
+    size_t right = length_of_palindrome == 0 ? 0 : length_of_palindrome - 1;
     while (left < right) {
         if (str[left] == ' ') {
             ++left;
