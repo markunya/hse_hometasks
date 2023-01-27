9064951031fac77bbe32875007c6916593be68de
Mark Bondarenko <vasya@pupkin.ru>
2023-01-27 19:47:06 +0300

    destroed str_length variable

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,9 +1,8 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    size_t length_of_palindrome = str.size();
     size_t left = 0;
-    size_t right = length_of_palindrome == 0 ? 0 : length_of_palindrome - 1;
+    size_t right = str.empty() ? 0 : str.size() - 1;
     while (left < right) {
         if (str[left] == ' ') {
             ++left;
