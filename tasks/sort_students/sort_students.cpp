cf5ef836c4ed665985077f9ece5d0ad4f0c66123
Mark Bondarenko <vasya@pupkin.ru>
2023-01-28 12:26:02 +0300

    tuple for tie

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -2,16 +2,12 @@
 #include "tuple"
 
 bool CompareStudentsWithDate(Student student_1, Student student_2) {
-    std::tuple<int, int, int, std::string, std::string> student_1_tuple(student_1.birth_date.year, student_1.birth_date.month, student_1.birth_date.day, student_1.last_name, student_1.name);
-    std::tuple<int, int, int, std::string, std::string> student_2_tuple(student_2.birth_date.year, student_2.birth_date.month, student_2.birth_date.day, student_2.last_name, student_2.name);
-    return student_1_tuple < student_2_tuple;
+    return std::tie(student_1.birth_date.year, student_1.birth_date.month, student_1.birth_date.day, student_1.last_name, student_1.name) < std::tie(student_2.birth_date.year, student_2.birth_date.month, student_2.birth_date.day, student_2.last_name, student_2.name);
 }
 
 bool CompareStudentsWithName(Student student_1, Student student_2) {
-        std::tuple<std::string, std::string, int, int, int> student_1_tuple(student_1.last_name, student_1.name, student_1.birth_date.year, student_1.birth_date.month, student_1.birth_date.day);
-        std::tuple<std::string, std::string, int, int, int> student_2_tuple(student_2.last_name, student_2.name, student_2.birth_date.year, student_2.birth_date.month, student_2.birth_date.day);
-        return student_1_tuple < student_2_tuple;
-    }
+    return std::tie(student_1.last_name, student_1.name, student_1.birth_date.year, student_1.birth_date.month, student_1.birth_date.day) < std::tie(student_2.last_name, student_2.name, student_2.birth_date.year, student_2.birth_date.month, student_2.birth_date.day);
+}
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
     if (sortKind == SortKind::Date) {
