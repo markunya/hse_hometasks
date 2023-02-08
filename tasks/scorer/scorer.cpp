2ca9f504cdb455fa6e3f5869e0aeab3ee14e848c
Mark Bondarenko <vasya@pupkin.ru>
2023-02-09 02:19:44 +0300

    Stupid attempt

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -7,7 +7,7 @@ bool CompareEvents(const Event* event_1, const Event* event_2) {
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable scored_students = {};
-    std::unordered_map<std::string, bool> students_merge;
+    std::unordered_map<std::string, bool> students_merge_requests;
     std::vector<const Event*> interesting_events = {};
     for (size_t i = 0; i < events.size(); ++i) {
         if (events[i].time <= score_time) {
@@ -17,14 +17,15 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::sort(interesting_events.begin(), interesting_events.end(), CompareEvents);
     for (size_t i = 0; i < interesting_events.size(); ++i) {
         if (interesting_events[i]->event_type == EventType::CheckSuccess &&
-            !students_merge[interesting_events[i]->student_name]) {
+            !students_merge_requests[interesting_events[i]->student_name]) {
             scored_students[interesting_events[i]->student_name].insert(interesting_events[i]->task_name);
         }
         if (interesting_events[i]->event_type == EventType::MergeRequestClosed) {
-            students_merge[interesting_events[i]->student_name] = false;
+            students_merge_requests[interesting_events[i]->student_name] = false;
+            scored_students[interesting_events[i]->student_name].insert(interesting_events[i]->task_name);
         }
         if (interesting_events[i]->event_type == EventType::MergeRequestOpen) {
-            students_merge[interesting_events[i]->student_name] = true;
+            students_merge_requests[interesting_events[i]->student_name] = true;
         }
     }
     return scored_students;  // test
