704fe8259f829039ee5e051f699a86140eceb6b1
Mark Bondarenko <vasya@pupkin.ru>
2023-02-09 01:29:57 +0300

    Code style

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -16,7 +16,8 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     }
     std::sort(interesting_events.begin(), interesting_events.end(), CompareEvents);
     for (size_t i = 0; i < interesting_events.size(); ++i) {
-        if (interesting_events[i]->event_type == EventType::CheckSuccess && !students_merge[interesting_events[i]->student_name]) {
+        if (interesting_events[i]->event_type == EventType::CheckSuccess &&
+            !students_merge[interesting_events[i]->student_name]) {
             scored_students[interesting_events[i]->student_name].insert(interesting_events[i]->task_name);
         }
         if (interesting_events[i]->event_type == EventType::MergeRequestClosed) {
@@ -26,5 +27,5 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
             students_merge[interesting_events[i]->student_name] = true;
         }
     }
-    return scored_students; // test
+    return scored_students;  // test
 }
