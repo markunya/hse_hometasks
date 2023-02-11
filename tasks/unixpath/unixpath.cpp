a2ce262216911f70e15b9a3ee48877f2d9cdaf1d
Mark Bondarenko <vasya@pupkin.ru>
2023-02-12 00:57:45 +0300

    First attempt

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,5 +1,28 @@
 #include "unixpath.h"
-
+#include "iostream"
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    return "";
+    std::string normalized_path = static_cast<std::string>(current_working_dir);
+    while (path.length() > 0) {
+        size_t index_of_slash = path.find("/");
+        if (index_of_slash == std::string_view::npos) {
+            std::string directory = static_cast<std::string>(path);
+            if (directory == "..") {
+                if (normalized_path == "/") {
+                    break;
+                }
+                normalized_path = normalized_path.substr(0,normalized_path.rfind("/"));
+            } else if (directory != ".") {
+                normalized_path += "/" + directory;
+            }
+            break;
+        }
+        std::string directory = static_cast<std::string>(path.substr(0, index_of_slash));
+        if (directory == "..") {
+            normalized_path = normalized_path.substr(0,normalized_path.rfind("/"));
+        } else if (directory != "." && !directory.empty()) {
+            normalized_path += "/" + directory;
+        }
+        path.remove_prefix(index_of_slash + 1);
+    }
+    return normalized_path;
 }
