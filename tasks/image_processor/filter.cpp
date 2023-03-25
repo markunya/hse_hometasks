51d45435dd39abbf21aeabbab6df28413f00ba16
Mark Bondarenko <vasya@pupkin.ru>
2023-03-25 23:31:17 +0300

    Create variable zero with type int64_t to avoid ll vs l in func max

--- tasks/image_processor/filter.cpp
+++ tasks/image_processor/filter.cpp
@@ -4,14 +4,15 @@
 void MatrixFilter::Apply(Image &image) {
     std::vector<std::vector<int>> matrix = GetMatrix();
     std::vector<std::vector<RGB>> new_image(image.Height(), std::vector<RGB>(image.Width()));
+    const int64_t zero = 0;
     for (size_t i = 0; i < image.Height(); ++i) {
         for (size_t j = 0; j < image.Width(); ++j) {
             for (int64_t y = -static_cast<int>(matrix.size()) / 2;  y < static_cast<int64_t>(matrix.size()) / 2 + 1; ++y) {
                 for (int64_t x = -static_cast<int>(matrix[0].size()) / 2;  x < static_cast<int64_t>(matrix[0].size()) / 2 + 1; ++x) {
                     int64_t matrix_x = x + static_cast<int>(matrix[0].size()) / 2;
                     int64_t matrix_y = y + static_cast<int>(matrix.size()) / 2;
-                    size_t image_y = std::max(0ll, std::min(static_cast<int64_t>(image.Height()) - 1, static_cast<int64_t>(i) + y));
-                    size_t image_x = std::max(0ll, std::min(static_cast<int64_t>(image.Width()) - 1, static_cast<int64_t>(j) + x));
+                    size_t image_y = std::max(zero, std::min(static_cast<int64_t>(image.Height()) - 1, static_cast<int64_t>(i) + y));
+                    size_t image_x = std::max(zero, std::min(static_cast<int64_t>(image.Width()) - 1, static_cast<int64_t>(j) + x));
                     new_image[i][j] += matrix[matrix_y][matrix_x] * image[image_y][image_x];
                 }
             }
