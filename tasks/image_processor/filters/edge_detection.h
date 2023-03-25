d597680f139ec6e4a3950b43540ea1e027fcecb9
Mark Bondarenko <vasya@pupkin.ru>
2023-03-25 23:37:52 +0300

    Code style

--- tasks/image_processor/filters/edge_detection.h
+++ tasks/image_processor/filters/edge_detection.h
@@ -5,8 +5,9 @@
 class EdgeDetection : public MatrixFilter {
 public:
     explicit EdgeDetection(const double& threshold);
-    void Apply(Image &image) override;
+    void Apply(Image& image) override;
     std::vector<std::vector<int>> GetMatrix() const final;
+
 private:
     const double threshold_;
     const std::vector<std::vector<int>> matrix_ = {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}};
