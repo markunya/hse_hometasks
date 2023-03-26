65af287e3f31f3cb0c3fe391b39fd851e663417b
Mark Bondarenko <vasya@pupkin.ru>
2023-03-26 05:29:50 +0300

    Code style

--- tasks/image_processor/filters/gausian_blur.h
+++ tasks/image_processor/filters/gausian_blur.h
@@ -7,6 +7,7 @@ public:
     explicit GaussianBlur(const double& sigma);
     void Apply(Image& image) override;
     const std::vector<std::vector<std::vector<double>>>& GetMatrixVector() const final;
+
 private:
     std::vector<std::vector<std::vector<double>>> matrix_vector_;
 };
